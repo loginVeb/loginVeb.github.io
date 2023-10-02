cf7a0221f56ed5daa44f0145f5c4cefbf4a77f30
loginVeb <loginVeb@yandex.ru>
2023-10-02 13:35:18 +0300

    rab npm run xxx

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,31 @@
 import { defineConfig } from "vite";
 import { VitePWA as pwa } from "vite-plugin-pwa";
-import manifest from "./locmanifest.json";
+
+
+import manifest from "./locmanifest.json" assert { type: "json" };
+//import manifest from "./locmanifest.json";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: { 
+    //https: true,
+    port: 3000
+  },
   plugins: [
-    react(),
+  react({
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            {
+              displayName: true,
+              fileName: false
+            }
+          ]
+        ]
+      }
+    })  ,
     pwa({
       strategies: "injectManifest",
       srcDir: "",
